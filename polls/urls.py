2bd43187bf1c414f3e6e5d45700ea4d5477ea94a
Jemart Pacilan <jpacilan@fullscale.io>
2019-12-12 01:46:01 +0800

    Move files to personal github account

---
+++ polls/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from . import views
+
+app_name="polls"
+urlpatterns = [
+    path('list/', views.polls_list, name="list"),
+    path('add/', views.add_poll, name='add'),
+    # polls/details/1/
+    path('details/<int:poll_id>/', views.poll_detail, name='detail'),
+    # polls/details/1/vote
+    path('details/<int:poll_id>/vote/', views.poll_vote, name='vote')
+]
